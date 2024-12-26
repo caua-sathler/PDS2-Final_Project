6fe543915d0a78a4a24e4101e36fc8983e9df653
Julialunna <juliaaluna1@gmail.com>
2024-12-26 12:50:57 -0300

    creating Game constructor

---
+++ include/Game.hpp
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game{
+    private:
+        int num_rows;
+        int num_columns;
+        int** game_board = nullptr;
+    public:
+        Game(int num_rows, int num_columns);
+        void create_game_board();
+};
+
+#endif
---
+++ src/Game.cpp
@@ -0,0 +1,11 @@
+#include "Game.hpp"
+void Game::create_game_board(){
+    this->game_board = new int*[num_rows];
+    for(int i=0;i<num_rows;i++){
+        this->game_board[i]=new int[num_columns];
+    }
+}
+
+Game::Game(int num_rows_received, int num_columns_received) : num_columns(num_columns_received), num_rows(num_rows_received){
+    create_game_board();
+}
