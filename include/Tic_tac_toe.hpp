e625d4efdccdc9e9203d49f1f9970ee3b3d1bc80
Julialunna <juliaaluna1@gmail.com>
2024-12-27 15:18:23 -0300

    Creating Reversi branch

--- include/Tic_tac_toe.hpp
+++ include/Tic_tac_toe.hpp
@@ -4,6 +4,7 @@
 #include "Game.hpp"
 
 class tic_tac_toe : public Game {
+    //Não precisa declara denovo, já foi herdado
     private:
         int num_rows;
         int num_columns;
@@ -11,6 +12,7 @@ class tic_tac_toe : public Game {
     public:
         tic_tac_toe(int num_rows_received, int num_columns_received);
         bool verify_valid_move(int x, int y) override;
+        //Make_move tá sem override
         void make_move(int x, int y);
         void check_win() override;
         ~tic_tac_toe();
