3583ae1b519a888de4636a8a695be80dd59f8452
Guilherme Farany <gfaranyufmg@gmail.com>
2024-12-27 10:57:31 -0300

    Creating tic-tac-toe's class constructor

---
+++ src/Tic_tac_toe.cpp
@@ -0,0 +1,18 @@
+#include "Tic_tac_toe.hpp"
+#include <iostream>
+
+tic_tac_toe::tic_tac_toe(int num_rows_received, int num_columns_received) : Game(num_rows_received, num_columns_received) {
+}
+
+void tic_tac_toe::verify_valid_move() {
+
+}
+
+void tic_tac_toe::make_move() {
+
+}
+
+void tic_tac_toe::check_win() {
+
+}
+
