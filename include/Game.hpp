3dedb7306695a02913acff1dd0cd7a858e58bcac
Julialunna <juliaaluna1@gmail.com>
2024-12-27 16:13:19 -0300

    starting reversi implementation

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,19 +1,21 @@
 #ifndef GAME_H
 #define GAME_H
 
-class Game {
-    protected:
+class Game{
+    private:
         int num_rows;
         int num_columns;
         char** game_board = nullptr;
-
     public:
         Game(int num_rows_received, int num_columns_received);
         void create_game_board();
         void print_game_board() const;
-        virtual bool verify_valid_move() const = 0;
+        virtual bool verify_valid_move() = 0;
         virtual void make_move() = 0;
-        virtual void check_win() const = 0;
+        virtual void check_win() = 0;
+        char** get_game_board() {
+            return game_board;
+        }
         ~Game();
 };
 
---
+++ include/Reversi.hpp
@@ -0,0 +1,11 @@
+#include "Game.hpp"
+#ifndef REVERSI_H
+#define REVERSI_H
+class Reversi : public Game{
+    private:
+        int num_pieces_player1;
+        int num_pieces_player2;
+    public:
+       
+};
+#endif
