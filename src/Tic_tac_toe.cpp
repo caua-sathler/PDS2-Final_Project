c38c34d5c98467dbb7d24dc70ef0735b4be071e8
Guilherme Farany <gfaranyufmg@gmail.com>
2024-12-28 11:51:10 -0300

    Creation of check-Win function on tic-tac-toe game

--- src/Tic_tac_toe.cpp
+++ src/Tic_tac_toe.cpp
@@ -34,7 +34,19 @@ void tic_tac_toe::switch_players() {
 }
 
 void tic_tac_toe::check_win() const {
+    for (int i = 0; i < 3; ++i) { 
+        if ((game_board[i][0] == current_player && game_board[i][1] == current_player && game_board[i][2] == current_player) || 
+            (game_board[0][i] == current_player && game_board[1][i] == current_player && game_board[2][i] == current_player)) {
+            std::cout << "Jogador " << current_player << " venceu!\n";
+            return;
+        }
+    }
 
+     if ((game_board[0][0] == current_player && game_board[1][1] == current_player && game_board[2][2] == current_player) || 
+        (game_board[0][2] == current_player && game_board[1][1] == current_player && game_board[2][0] == current_player)) {
+        std::cout << "Jogador " << current_player << " venceu!\n";
+        return;
+    }
 }
 
 tic_tac_toe::~tic_tac_toe() {
