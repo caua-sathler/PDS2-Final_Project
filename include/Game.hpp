ae2de7839986069255159aefe866d7a7a195913f
Julialunna <juliaaluna1@gmail.com>
2024-12-28 15:07:27 -0300

    Updating class Game

--- include/Game.hpp
+++ include/Game.hpp
@@ -1,11 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
-class Game{
-    private:
+class Game {
+    protected:
         int num_rows;
         int num_columns;
         char** game_board = nullptr;
+
     public:
         Game(int num_rows_received, int num_columns_received);
         void create_game_board();
@@ -13,10 +14,7 @@ class Game{
         virtual bool is_valid_move() const = 0;
         virtual bool is_move_inside_board(int x, int y) const;
         virtual void make_move() = 0;
-        virtual void check_win() = 0;
-        char** get_game_board() {
-            return game_board;
-        }
+        virtual void check_win() const = 0;
         ~Game();
 };
 
