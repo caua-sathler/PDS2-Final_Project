dfd6b95e3e7166e0a39d9aa6be560448cc548059
Arthur Faria <afariagoncalves72@gmail.com>
2024-12-29 14:13:49 -0300

    Created setters and default constructor

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,9 +1,27 @@
 #include "Player.hpp"
 #include <iostream>
-#include <list>
+#include <map>
+
+Player::Player(): Player("", "", {} , {}) {};
 
 Player::Player(std::string name_received, std::string username_received, std::map<char, int> num_win_received, std::map<char, int> num_loss_received):
    name(name_received), username(username_received), num_win(num_win_received), num_loss(num_loss_received) {};
+
+void Player::set_name(std::string name_received){
+    this->name = name_received;
+}    
+
+void Player::set_username(std::string username_received){
+    this->username = username_received;
+}
+
+void Player::set_num_win(char key, int value){
+    this->num_win.insert_or_assign(key, value);
+}
+
+void Player::set_num_loss(char key, int value){
+    this->num_loss.insert_or_assign(key,value);
+}
 
 void Player::register_player(std::string name_received, std::string username_received){
 
