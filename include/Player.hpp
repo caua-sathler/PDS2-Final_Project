043fb2662a31be18ec0216b11c344aaa6d4826af
Arthur Faria <afariagoncalves72@gmail.com>
2024-12-30 13:54:18 -0300

    changed map key to string

--- include/Player.hpp
+++ include/Player.hpp
@@ -8,17 +8,17 @@ class Player {
     private:
         std::string name;
         std::string username;
-        std::map<char, int> num_win;
-        std::map<char, int> num_loss;
+        std::map<std::string, int> num_win;
+        std::map<std::string, int> num_loss;
 
     public:
         Player();
-        Player(std::string name_received, std::string username_received, std::map<char, int> num_win_received, std::map<char, int> num_loss_received);
+        Player(std::string name_received, std::string username_received, std::map<std::string, int> num_win_received, std::map<std::string, int> num_loss_received);
 
         void set_name(std::string name_received);
         void set_username(std::string username_received);
-        void set_num_win(char key, int value);
-        void set_num_loss(char key, int value);
+        void set_num_win(std::string key, int value);
+        void set_num_loss(std::string key, int value);
         
         void register_player(std::string name_received, std::string username_received);
         void remove_player();
--- src/Player.cpp
+++ src/Player.cpp
@@ -4,7 +4,7 @@
 
 Player::Player(): Player("", "", {} , {}) {};
 
-Player::Player(std::string name_received, std::string username_received, std::map<char, int> num_win_received, std::map<char, int> num_loss_received):
+Player::Player(std::string name_received, std::string username_received, std::map<std::string, int> num_win_received, std::map<std::string, int> num_loss_received):
    name(name_received), username(username_received), num_win(num_win_received), num_loss(num_loss_received) {};
 
 void Player::set_name(std::string name_received){
@@ -15,11 +15,11 @@ void Player::set_username(std::string username_received){
     this->username = username_received;
 }
 
-void Player::set_num_win(char key, int value){
+void Player::set_num_win(std::string key, int value){
     this->num_win.insert_or_assign(key, value);
 }
 
-void Player::set_num_loss(char key, int value){
+void Player::set_num_loss(std::string key, int value){
     this->num_loss.insert_or_assign(key,value);
 }
 
@@ -33,7 +33,7 @@ void Player::remove_player(){
 
 void Player::print_player(){
     std::cout << this->username << " " << this->name << std::endl;
-    std::cout << "REVERSI" << "\t" << "- V: " << this->num_win.find('R')->second << " D: " << this->num_loss.find('R')->second << std::endl;
-    std::cout << "LIG4" << "\t" << "- V: " << this->num_win.find('L')->second << " D: " << this->num_loss.find('L')->second << std::endl;
-    std::cout << "VELHA" << "\t" << "- V: " << this->num_win.find('V')->second << " D: " << this->num_loss.find('V')->second << std::endl;
+    std::cout << "REVERSI" << "\t" << "- V: " << this->num_win.find("Reversi")->second << " D: " << this->num_loss.find("Reversi")->second << std::endl;
+    std::cout << "LIG4" << "\t" << "- V: " << this->num_win.find("Lig4")->second << " D: " << this->num_loss.find("Lig4")->second << std::endl;
+    std::cout << "VELHA" << "\t" << "- V: " << this->num_win.find("Velha")->second << " D: " << this->num_loss.find("Velha")->second << std::endl;
 }
