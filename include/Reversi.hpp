b20ce8e01de18833f33eb6f6e04b9c85096f16ac
Julialunna <juliaaluna1@gmail.com>
2025-01-05 09:40:00 -0300

    Making is_valid_move adhere to the single responsibility principle and adapting make_move

--- include/Reversi.hpp
+++ include/Reversi.hpp
@@ -11,18 +11,19 @@ class Reversi : public Game {
     public:
         Reversi();
         void start_reversi_board();
-        bool is_valid_move() const override;
-        bool is_valid_move(std::array<int, 2>& move_coordinates, char player_piece_type, std::list<std::array<int, 2>>& squares_to_make_move);
         bool is_space_free(int x, int y) const;
         bool is_move_inside_board(int x, int y) const override;
         bool is_there_player_piece_at_the_direction(const char player_piece, const std::array<int, 2>& direction, 
              std::array<int, 2> adjacent_square) const;
+        bool is_there_direction_that_captures_opponent(const std::array<int, 2> &move_coordinates, char player_piece_type);
+        bool is_valid_move() const override;
+        bool is_valid_move(std::array<int, 2>& move_coordinates, char player_piece_type);
+        void find_all_directions_to_make_move(std::array<int, 2>& move_coordinates, char player_piece, std::list<std::array<int, 2>>&directions_to_capture_opponents);
         void flip_pieces(std::array<int, 2> directions, std::array<int, 2> move_coordinates, char player_piece);
         void make_move() override;
         void make_move(std::array<int, 2> move_coordinates, char player_piece, std::list<std::array<int, 2>>& directions_to_capture_opponents); 
-        void check_win() const override; 
-        void find_directions_that_make_moves_against_opponent(const std::array<int, 2>& move_coordinates, char player_piece_type, std::list<std::array<int, 2>>& squares_to_make_move);
         bool process_move(std::array<int, 2> move_coordinates, char player_piece_type);
+        void check_win() const override; 
 };
 
 #endif
