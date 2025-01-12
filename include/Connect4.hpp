1b0ecfa81ac70995f945ea53bea60a473092a1a9
Leonardo Mendes <leoantero.m@gmail.com>
2025-01-12 18:52:34 -0300

    Updated connect4 class for the changes made in game and the new board class, i also changed make move and check win to make implementation in main easier

--- include/Connect4.hpp
+++ include/Connect4.hpp
@@ -7,19 +7,17 @@
 class Connect4 : public Game {
     private:
         char current_player;
-        char winner;
 
     public:
         Connect4();
-        bool is_move_inside_board(int = 0, int) const override;
         bool is_valid_move() const override;
         bool is_valid_move(int column) const; //Sobrecarga
         void make_move() override;
         void make_move(int column); //Sobrecarga
-        void switch_players();
         bool check_win();
         char get_current_player() const;
         bool is_board_full() const;
+        void print_game_board() const;
         ~Connect4();
 };
 
--- src/Connect4.cpp
+++ src/Connect4.cpp
@@ -4,18 +4,11 @@
 Connect4::Connect4() : Game(6, 7), current_player('X'){
 }
 
-bool Connect4::is_move_inside_board(int a, int column) const {
-    if (column < 1 || column > 7) { 
-        //Exceptions and error handling, fix
-        return false;
-    }
-    return true;
-}
-
 // If the move is valid return true, otherwise return false
 bool Connect4::is_valid_move(int column) const {
     column--; 
-    if (game_board[0][column] != ' ' || !is_move_inside_board(column)) {
+    int rows = 6;
+    if (game_board.get_space(0, column) != ' ' || !game_board.is_move_inside_board(rows, column)) {
         //Exceptions and error handling, fix 
         return false;
     }
@@ -24,23 +17,13 @@ bool Connect4::is_valid_move(int column) const {
 
 
 void Connect4::make_move(int column) {
-    if(is_valid_move(column)) {
         column--;
         for(int i = 5; i >= 0; i--) {
-            if(game_board[i][column] == ' ') {
-                game_board[i][column] = current_player;
+            if(game_board.get_space(i, column) == ' ') {
+                game_board.set_space(i, column, current_player);
                 return;
             }
         }
-    }
-}
-
-void Connect4::switch_players(){
-    if(current_player == 'X') {
-        current_player = 'O';
-    } else {
-        current_player = 'X';
-    }
 }
 
 bool Connect4::check_win(){
@@ -50,10 +33,10 @@ bool Connect4::check_win(){
 
     for (int row = 0; row < 6; ++row) {
         for (int col = 0; col <= 3; ++col) {
-            if (game_board[row][col] == current_player && 
-                game_board[row][col + 1] == current_player &&
-                game_board[row][col + 2] == current_player && 
-                game_board[row][col + 3] == current_player) {
+            if (game_board.get_space(row, col) == current_player && 
+                game_board.get_space(row, col + 1) == current_player &&
+                game_board.get_space(row, col + 2) == current_player && 
+                game_board.get_space(row, col + 3) == current_player) {
                     return true;
             }
         }
@@ -63,10 +46,10 @@ bool Connect4::check_win(){
 
     for (int row = 0; row <= 2; ++row) {
         for (int col = 0; col < 7; ++col) {
-            if (game_board[row][col] == current_player && 
-                game_board[row+1][col] == current_player &&
-                game_board[row + 2][col] == current_player && 
-                game_board[row + 3][col] == current_player) {
+            if (game_board.get_space(row, col) == current_player && 
+                game_board.get_space(row + 1, col) == current_player &&
+                game_board.get_space(row + 2, col) == current_player && 
+                game_board.get_space(row + 3, col) == current_player) {
                     return true;
             }
         }
@@ -76,10 +59,10 @@ bool Connect4::check_win(){
 
     for (int row = 0; row <= 2; ++row) {
         for (int col = 0; col <= 3; ++col) {
-            if (game_board[row][col] == current_player && 
-                game_board[row+1][col+1] == current_player &&
-                game_board[row + 2][col+2] == current_player && 
-                game_board[row + 3][col+3] == current_player) {
+            if (game_board.get_space(row, col) == current_player && 
+                game_board.get_space(row + 1, col + 1) == current_player &&
+                game_board.get_space(row + 2, col + 2) == current_player && 
+                game_board.get_space(row + 3, col + 3) == current_player) {
                     return true;
             }
         }
@@ -87,12 +70,12 @@ bool Connect4::check_win(){
 
     // left diagonal check
 
-    for (int row = 0; row <= 2; ++row) {
+    for (int row = 3; row < 6; ++row) {
         for (int col = 0; col <= 3; ++col) {
-            if (game_board[row][col] == current_player && 
-                game_board[row-1][col-1] == current_player &&
-                game_board[row - 2][col - 2] == current_player && 
-                game_board[row - 3][col - 3] == current_player) {
+            if (game_board.get_space(row, col) == current_player && 
+                game_board.get_space(row - 1, col + 1) == current_player &&
+                game_board.get_space(row - 2, col + 2) == current_player && 
+                game_board.get_space(row - 3, col + 2) == current_player) {
                     return true;
             }
         }
@@ -105,12 +88,16 @@ char Connect4::get_current_player() const {
 }
 
 bool Connect4::is_board_full() const{
-    for(int col = 0; col < num_columns; ++col) {
-        if(game_board[0][col] == ' ') {
+    for(int col = 0; col < 7; ++col) {
+        if(game_board.get_space(0, col) == ' ') {
             return false;
         }
     }
     return true;
+}
+
+void Connect4::print_game_board() const{
+    game_board.print_game_board();
 }
 
 Connect4::~Connect4() {
