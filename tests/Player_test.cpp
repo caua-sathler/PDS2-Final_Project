2b8d618b03390d7c8cef2d1ce9ee4864333cf79b
Arthur Faria <afariagoncalves72@gmail.com>
2025-01-27 08:55:50 -0300

    created tests for methods add_win and add_loss

--- tests/Player_test.cpp
+++ tests/Player_test.cpp
@@ -32,3 +32,56 @@ TEST_CASE("Teste método register_player")
     delete player4;
 }
 
+TEST_CASE("Teste método add_win"){
+    Player player;
+    player.add_win("Reversi");
+    player.add_win("Lig4");
+    player.add_win("Velha");
+    std::map<std::string, int> win_test = {{"Reversi", 1}, {"Lig4", 1}, {"Velha", 1}};
+    CHECK(player.get_num_win() == win_test);
+
+    player.set_num_win("Velha", -13);
+    player.add_win("Velha");
+    win_test["Velha"] = -12;
+    CHECK(player.get_num_win() == win_test);
+
+    player.set_num_win("Reversi", 2147483647);
+    player.add_win("Reversi");
+    win_test["Reversi"] = 2147483648;
+    CHECK(player.get_num_win() == win_test);
+
+    player.set_num_win("Lig4", -2147483648);
+    player.add_win("Lig4");
+    win_test["Lig4"] = -2147483647;
+    CHECK(player.get_num_win() == win_test);
+
+    player.add_win("Game");
+    CHECK(player.get_num_win() == win_test);
+}
+
+TEST_CASE("Teste método add_loss"){
+    Player player;
+    player.add_loss("Reversi");
+    player.add_loss("Lig4");
+    player.add_loss("Velha");
+    std::map<std::string, int> loss_test = {{"Reversi", 1}, {"Lig4", 1}, {"Velha", 1}};
+    CHECK(player.get_num_loss() == loss_test);
+
+    player.set_num_loss("Velha", -13);
+    player.add_loss("Velha");
+    loss_test["Velha"] = -12;
+    CHECK(player.get_num_loss() == loss_test);
+
+    player.set_num_loss("Reversi", 2147483647);
+    player.add_loss("Reversi");
+    loss_test["Reversi"] = 2147483648;
+    CHECK(player.get_num_loss() == loss_test);
+
+    player.set_num_loss("Lig4", -2147483648);
+    player.add_loss("Lig4");
+    loss_test["Lig4"] = -2147483647;
+    CHECK(player.get_num_loss() == loss_test);
+
+    player.add_loss("Game");
+    CHECK(player.get_num_loss() == loss_test);
+}
