c381542e6d25b4787998c6fb40f78ebad5b1f0ce
Guilherme Farany <gfaranyufmg@gmail.com>
2025-01-26 23:29:40 -0300

    Basic tests of valid move on tic-tac-toe game

--- tests/Tic-tac-toeClass_teste.cpp
+++ tests/Tic-tac-toeClass_teste.cpp
@@ -10,4 +10,31 @@ void create_tic_tac_toe_board(tic_tac_toe &game, const std::vector<std::vector<c
     }
 }
 
+TEST_CASE("Testes da classe Tic_tac_toe") 
+{
+    tic_tac_toe jogo;
 
+    SUBCASE("Verificar jogador atual ao iniciar o jogo") 
+        CHECK(jogo.get_current_player() == 'X');
+    
+
+    SUBCASE("Verificar se uma jogada é válida") 
+    {
+        int x = 1, y = 1; 
+        CHECK(jogo.is_valid_move(x, y) == true);
+
+        x = 4, y = 4; 
+        CHECK(jogo.is_valid_move(x, y) == false);
+
+        x = 1, y = 1;
+        jogo.make_move(x, y); 
+        CHECK(jogo.is_valid_move(x, y) == false);
+    }
+
+    SUBCASE("Realizar uma jogada válida") 
+    {
+        int x = 2, y = 2;
+        jogo.make_move(x, y);
+        CHECK(jogo.get_current_player() == 'O'); 
+    }
+}
